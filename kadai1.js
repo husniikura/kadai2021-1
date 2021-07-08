4ffc9f1913221a3c1ceabdcd0d9f7ed469f56122
kawaabtamayu <2195032@stu.hus.ac.jp>
2021-07-08 14:13:38 +0900

    修正しました

--- kadai1.js
+++ kadai1.js
@@ -5,18 +5,15 @@ console.log("Take one down and pass it around, 98 bottles of beer on the wall.")
 console.log("98 bottles of beer on the wall, 98 bottles of beer.")
 console.log("Take one down and pass it around, 97 bottles of beer on the wall.")
 
-console.log("中略")
+console.log("97 bottles of beer on the wall, 97 bottles of beer.")
+console.log("Take one down and pass it around, 96 bottles of beer on the wall.")
 
-console.log("1 bottle of beer on the wall, 1 bottle of beer.")
-console.log("Take one down and pass it around, no more bottles of beer on the wall.")
 
-console.log("No more bottles of beer on the wall, no more bottles of beer.")
-console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
-console.log("99 bottles of beer on the wall, 99 bottles of beer.")
-console.log("Take one down and pass it around, 98 bottles of beer on the wall.")
+console.log("96 bottles of beer on the wall, 96 bottles of beer.")
+console.log("Take one down and pass it around, 95 bottles of beer on the wall.")
 
-console.log("98 bottles of beer on the wall, 98 bottles of beer.")
-console.log("Take one down and pass it around, 97 bottles of beer on the wall.")
+console.log("95 bottles of beer on the wall, 95 bottles of beer.")
+console.log("Take one down and pass it around, 94 bottles of beer on the wall.")
 
 console.log("中略")
 
