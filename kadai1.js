998ab053eee65e531f98a0369744eb5ce24b65ce
kawaabtamayu <2195032@stu.hus.ac.jp>
2021-07-08 14:17:50 +0900

    修正しました2

--- kadai1.js
+++ kadai1.js
@@ -1,7 +1,7 @@
 
 console.log("99 bottles of beer on the wall, 99 bottles of beer.")
 console.log("Take one down and pass it around, 98 bottles of beer on the wall.")
-
+console.log("")
 console.log("98 bottles of beer on the wall, 98 bottles of beer.")
 console.log("Take one down and pass it around, 97 bottles of beer on the wall.")
 
