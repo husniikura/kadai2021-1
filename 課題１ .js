7de1c3d856a8ec7b78eb1463fa401193a244ee77
shoka-honmo <2195002@stu.hus.ac.jp>
2021-07-31 14:19:53 +0900

    提出

---
+++ 課題１ .js
@@ -0,0 +1,11 @@
+let i;
+for(i=99;0<i;i--){
+   if(i==1){
+       console.log("1 bottle of beer on the wal1, 1 bottle of beer.");    
+   }else{
+    console. log(i+"bottles of beer on the wall, "+(i-1)+" bottles of beer.    ");
+   } 
+}
+
+console.log("No more bottles of beer on the wall, no more bottles of beer.");
+console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
