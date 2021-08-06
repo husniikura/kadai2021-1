a7cc0510133760285776f72e61fd1cf423f79260
kawaabtamayu <2195032@stu.hus.ac.jp>
2021-08-06 23:30:48 +0900

    修正しました９

--- kadai1.js
+++ kadai1.js
@@ -9,11 +9,13 @@ for(i= 99;0 <i;i--){
        console.log("Take one down and pass it around, 1 bottle of beer on the wall."); 
        console.log("") 
     } 
-    console.log( i +" bottles of beer on the wall, "+(i)+" bottles of beer."); 
-    console.log("Take one down and pass it around, "+(i-1)+" bottles of beer on the wall.");
-    console.log("")    
- }
+      }else{
+       console.log( i +" bottles of beer on the wall, "+(i)+" bottles of beer."); 
+       console.log("Take one down and pass it around, "+(i-1)+" bottles of beer on the wall.");
+       console.log("")    
+    }
+    
 }
+
 console.log("No more bottles of beer on the wall, no more bottles of beer.");
-console.log("Go to the store and buy some more, 99 bottles of beer on the wall."); 
-console.log("Go to the store and buy some more, 99 bottles of beer on the wall.") ;
+console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")  
